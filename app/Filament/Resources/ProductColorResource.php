d89b2c7aed295b3ac20c280f9ba66b938a9ff0c2
Harshadeva <harshadeva@webmotech.com>
2025-07-25 17:34:01 +0530

    Create Product Color Table

---
+++ app/Filament/Resources/ProductColorResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductColorResource\Pages;
+use App\Filament\Resources\ProductColorResource\RelationManagers;
+use App\Models\ProductColor;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Support\Enums\Alignment;
+use Filament\Tables;
+use Filament\Tables\Columns\ColorColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductColorResource extends Resource
+{
+    protected static ?string $model = ProductColor::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->toggleable()
+                    ->sortable()
+                    ->searchable(),
+
+
+                TextColumn::make('hex_code')
+                    ->label('Hex Code')
+                    ->sortable()
+                    ->toggleable(),
+
+                ColorColumn::make('hex_code_color')
+                    ->label('Color')
+                    ->toggleable()
+                      ->alignment(Alignment::Center)
+                    ->getStateUsing(fn(ProductColor $record): string => $record->hex_code)
+                    ->tooltip(fn (ProductColor $record): string => "Created At :  {$record->created_at->format('F j, Y, g:i a')}"),
+            ])
+            ->filters([])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])->searchPlaceholder('Search (Name)')
+             ->defaultSort('created_at', 'desc');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProductColors::route('/'),
+            'create' => Pages\CreateProductColor::route('/create'),
+            'edit' => Pages\EditProductColor::route('/{record}/edit'),
+        ];
+    }
+}
